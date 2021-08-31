48757cde38d76f6400e6e8423b3c1ba882301b98
Kenneth Rusli <71806609+Krus5li@users.noreply.github.com>
2021-08-31 17:04:36 -0500

    first file drops

---
+++ script.js
@@ -0,0 +1,9 @@
+import RBT from './algorithms/redblacktree.js';
+import Node from './algorithms/redblacktree.js';
+
+function test() {
+    let rbt = new RBT()
+    rbt.insert(2)
+    rbt.insert(3)
+    rbt.insert(1)
+}
